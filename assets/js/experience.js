a01803f04f0b20e98ea14bcc74d90d9500831435
Akash Bangalkar <44271498+akashbangalkar@users.noreply.github.com>
2022-03-23 00:45:43 +0530

    Update experience.js

--- assets/js/experience.js
+++ assets/js/experience.js
@@ -6,10 +6,17 @@ AOS.init();
 const experiencecards = document.querySelector(".experience-cards");
 const exp = [
   {
-    title: "Applied Machine Learning Scholar",
+    title: "Salesforce Consultant",
+    cardImage: "assets/images/experience-page/stratos.jpg",
+    place: "Stratos Studios",
+    time: "(February, 2022 - Present)",
+    desp: "",
+  },
+  {
+    title: "Applied Machine Learning Scholar (Engineer)",
     cardImage: "assets/images/experience-page/ml.png",
     place: "Applied Ai",
-    time: "(August, 2021 - Present)",
+    time: "(August, 2021 - February, 2022)",
     desp: "<li>Studied the core conceps and Ideas in Machine Learning, Data Science and AI that would help to go from a real-world business problem to a first cut, working and deployable AI solution to the problem.</li> <li>Worked on several assignments and real-world case studies, which helps to grasp the practical details of building AI solutions. Knowledge on machine learning Data Processing steps like Data Exploration, Feature Cleaning, Feature Selection, Feature Encoding to construct a Data-set to apply various machine learning algorithms.</li> <li>Hands-on-knowledge on various algorithms like Linear & Logistic Regression, Random Forest, Decision Trees, Support Vector Machines, K-Nearest Neighbors, K-Means and Naive Bayes. Developing my skills by building machine learning models on various datasets available in Kaggle using various libraries in Python.</li>",
   },
   {
@@ -17,7 +24,7 @@ const exp = [
     cardImage: "assets/images/experience-page/dhan.png",
     place: "Dhandhania Infotech Pvt Ltd",
     time: "(Aug, 2019 - Jul, 2021)",
-    desp: "<li>Worked on different of projects with several teams and responsible for implementing a plan with errors being fixed, involved in planning and implementing strategies for quality management.</li><li>Along with that, involved in Data Analysis - Performing analysis to assess quality and meaning of data, Preparing final analysis reports, Fixing coding errors and related problems and maintaining databases, data systems. </li><li>Track weekly data to chart whether the departments hit goals. Produced reports and presentations using software.</li>",
+    desp: "<li>Worked on different of projects with several teams and responsible for implementing a plan with errors being fixed, involved in planning and implementing strategies for quality management.</li><li>Along with that, involved in Data Analysis - Performing analysis to assess quality and meaning of data, Preparing final analysis reports, Fixing coding errors (Python) and related problems and maintaining databases, data systems.</li><li>Experience in Python Development, Information Processing, Data Visualization and Analysis.</li><li>Track weekly data to chart whether the departments hit goals. Produced reports and presentations using software.</li>",
   },
   
 ];
